bff628542b54d2cdcc0a1f5992832b6b7c97da57
nm000 <mendozanatalia0827@gmail.com>
2024-11-11 20:30:46 -0500

    fix: model prediction initially added

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,9 @@ from flask_cors import CORS  # Importa CORS
 import cv2
 import numpy as np
 import base64
+import requests
+import io
+from datetime import datetime
 
 app = Flask(__name__)
 CORS(app)
@@ -13,6 +16,40 @@ CLIENT = InferenceHTTPClient(
     api_url="https://outline.roboflow.com",
     api_key="Glwg3WRKqF07dJBzArAn"
 )
+
+
+TELEGRAM_BOT_TOKEN = '7826884820:AAH1eOui_gbDF5s3ZsT7nfZEI6QgfTONPEE'
+TELEGRAM_CHAT_ID = '1577016023'
+
+def send_telegram_message(image_base64=None, drug_type=None, confidence=None):
+
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    message = f"¡Alerta! Se ha detectado una posible sustancia.\n"
+    message += f"Hora: {current_time}\n"
+    message += f"Tipo de Droga: {drug_type}\n" if drug_type else "Tipo de Droga: No disponible\n"
+    message += f"Confianza: {confidence * 100:.2f}%\n" if confidence else "Confianza: No disponible\n"
+    
+    # Enviar el mensaje de alerta
+    requests.post(
+        f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage',
+        data={'chat_id': TELEGRAM_CHAT_ID, 'text': message}
+    )
+
+    # Enviar la imagen si se proporciona en base64
+    if image_base64:
+        # Decodificar la imagen base64 a bytes
+        image_data = base64.b64decode(image_base64)
+        # Crear un objeto de archivo en memoria (sin necesidad de guardar en disco)
+        image_file = io.BytesIO(image_data)
+        
+        # Enviar la imagen como archivo
+        requests.post(
+            f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendPhoto',
+            data={'chat_id': TELEGRAM_CHAT_ID},
+            files={'photo': image_file}
+        )
+
 
 def detect_drugs(image_data):
     # Decodifica la imagen desde base64
@@ -34,9 +71,14 @@ def detect_drugs(image_data):
             label = f"{det['class']} ({det['confidence']*100:.1f}%)"
             cv2.putText(frame, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
 
+            clase = det['class']
+            confianza = det['confidence']
+
         # Codifica la imagen de vuelta a base64 para enviarla al frontend
         _, buffer = cv2.imencode('.jpg', frame)
         image_base64 = base64.b64encode(buffer).decode('utf-8')
+
+        send_telegram_message(image_base64=image_base64, drug_type=clase, confidence=(confianza/100))
         return {'detected': True, 'image': image_base64}
     return {'detected': False}
 
